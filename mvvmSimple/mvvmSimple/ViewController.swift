9d7019c79a4905b872af4e08700223147d219f61
DobruyPK <65114650+DobruyPK@users.noreply.github.com>
2023-09-25 13:04:04 +0300

    noSB

---
+++ mvvmSimple/mvvmSimple/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  mvvmSimple
+//
+//  Created by Семен Трифонов on 25.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
